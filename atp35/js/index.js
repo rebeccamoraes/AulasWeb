f8e9f06ff16cfb095697672be08c73e2e0bd0ab1
Rebecca Moraes <rebeccagabrielle@gmail.com>
2021-11-01 12:03:51 -0300

    ATP35 - Crianco página principal, definindo estilo e navegação.

---
+++ atp35/js/index.js
@@ -0,0 +1,24 @@
+function home() {
+    let frame = document.getElementById("frame");
+    frame.src = "home.html";
+}
+
+function resumo() {
+    let frame = document.getElementById("frame");
+    frame.src = "resumo.html";
+}
+
+function experiencias() {
+    let frame = document.getElementById("frame");
+    frame.src = "experiencias.html";
+}
+
+function habilidades() {
+    let frame = document.getElementById("frame");
+    frame.src = "habilidades.html";
+}
+
+function contatos() {
+    let frame = document.getElementById("frame");
+    frame.src = "contatos.html";
+}
